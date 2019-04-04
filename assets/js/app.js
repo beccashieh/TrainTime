3cffdf3ba85afa4893fdfd0c88efdcd7c592bca4
BShieh <rebeccasshieh@gmail.com>
2019-04-04 16:35:55 -0400

    wip: initial setup

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+//populate name, destination, frequency, arrival, and minutes away for 5 automatically.
+//enable search by name, destination, first time, and frequency upon submission.
+    //train data will populate in current train schedule field.
+
+
+
+
+});
